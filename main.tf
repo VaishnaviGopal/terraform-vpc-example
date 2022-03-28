baeebb6a6f49c521d71164a3dfd7e09b8e094010
VaishnaviGopal <guvaish1@in.ibm.com>
2022-03-28 15:28:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
 provider "ibm" {
-  generation       = 2
   region           = "us-south"
 }
 
@@ -15,4 +21,19 @@ resource "ibm_is_security_group_rule" "worker-node-inbound" {
     port_min = 30000
     port_max = 32767
   }
+}
+
+output "sg_rule_id" {
+value = {
+			   "is_resource" = "true"
+			   "resource_controller_url" = "<resource_url>"
+			   "resource_icon_url" = "<>"
+			   "resource_id" = "<resource_id>"
+			   "resource_name" = "<itemname>"
+         "original_value" = var.condition ? 1 : 0
+		}
+}
+
+variable "condition" {
+default = true
 }
